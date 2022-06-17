44465c00a8dec69ec219c066b97c6e68197644cf
ge93lej <christoph.rotte@tum.de>
2022-06-17 15:51:16 +0200

    ~

--- src/util/ErrorHandler.h
+++ src/util/ErrorHandler.h
@@ -1,11 +1,21 @@
 #ifndef B_EPSILON_ERRORHANDLER_H
 #define B_EPSILON_ERRORHANDLER_H
 // --------------------------------------------------------------------------
+#include <iostream>
 #include <string>
+#include <thread>
 // --------------------------------------------------------------------------
 namespace util {
 // --------------------------------------------------------------------------
-void raise(const std::string&);
+inline void raise(const std::string& what) {
+    std::cerr << "Thread ["
+              << std::this_thread::get_id()
+              << "] raised an exception: \""
+              << what
+              << "\""
+              << std::endl;
+    throw std::runtime_error(what);
+}
 // --------------------------------------------------------------------------
 }// namespace util
 // --------------------------------------------------------------------------
